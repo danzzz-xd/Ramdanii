d0d2f4e16b01a074ce0fbb24b26ef6d3d6690b8a
Ramdani Official <100227892+Ramdaniofficial@users.noreply.github.com>
2022-02-23 19:10:26 +0700

    Update main.js

--- main.js
+++ main.js
@@ -199,7 +199,7 @@ console.log(color(`${spc4}                       < =============================
             	num = anu.participants[0]
                 let v = Ramdani.contacts[num] || { notify: num.replace(/@.+/, '') }
                 anu_user = v.vname || v.notify || num.split('@')[0]
-                let buff = await getBuffer(`https://api.lolhuman.xyz/api/base/welcome?apikey=928afd11993f5ee29d5798f0&img1=${ppimg}&img2=${pp_grup}&background=https://telegra.ph/file/fcb183f0e234722c1a8d2.jpg&username=${encodeURI(anu_user)}&member=${memeg}&groupname= ${encodeURI(mdata.subject)}`)
+                let buff = await getBuffer(`https://api.lolhuman.xyz/api/base/welcome?apikey=SAYULONTEH&img1=${ppimg}&img2=${pp_grup}&background=https://telegra.ph/file/fcb183f0e234722c1a8d2.jpg&username=${encodeURI(anu_user)}&member=${memeg}&groupname= ${encodeURI(mdata.subject)}`)
                 masuk = `Halo @${num.split('@')[0]}\nSelamat Datang Di ${mdata.subject}`
                 gbutsan = [{ buttonId: 'SERAH', buttonText: { displayText: '👋Welcome' }, type: 1 }]
                 mhan = await Ramdani.prepareMessage(mdata.id, buff, MessageType.image, { thumbnail: buff })
@@ -230,7 +230,7 @@ console.log(color(`${spc4}                       < =============================
             	num = anu.participants[0]
                 let v = Ramdani.contacts[num] || { notify: num.replace(/@.+/, '') }
                 anu_user = v.vname || v.notify || num.split('@')[0]
-                let buff = await getBuffer(`https://api.lolhuman.xyz/api/base/leave?apikey=928afd11993f5ee29d5798f0&img1=${ppimg}&img2=${pp_grup}&background=https://telegra.ph/file/fcb183f0e234722c1a8d2.jpg&username=${encodeURI(anu_user)}&member=${memeg}&groupname=${encodeURI(mdata.subject)}`)
+                let buff = await getBuffer(`https://api.lolhuman.xyz/api/base/leave?apikey=SAYULONTEH&img1=${ppimg}&img2=${pp_grup}&background=https://telegra.ph/file/fcb183f0e234722c1a8d2.jpg&username=${encodeURI(anu_user)}&member=${memeg}&groupname=${encodeURI(mdata.subject)}`)
                 keluar = `Selamat tinggal @${num.split('@')[0]}\nSemoga tenang disana`
                 gbutsan = [{ buttonId: 'SERAH', buttonText: { displayText: '👋Byee' }, type: 1 }]
                 mhan = await Ramdani.prepareMessage(mdata.id, buff, MessageType.image, { thumbnail: buff })
